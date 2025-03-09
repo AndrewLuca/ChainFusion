252db811283c6b5df842ca86ef56480d16d3323a
Andrew Luca <andrew.d.luca@gmail.com>
2025-03-09 15:49:01 -0700

    start project

---
+++ src/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='bg-red-500'>
+            <h1>Home</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
